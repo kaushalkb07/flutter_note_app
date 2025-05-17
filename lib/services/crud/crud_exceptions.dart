8b38382edd4e1910ae64a8adacff3b01b3462fd9
kaushalkb07 <iamkaushalkb@gmail.com>
2025-05-17 23:35:25 +0545

    [Add] CRUD Operation

---
+++ lib/services/crud/crud_exceptions.dart
@@ -0,0 +1,17 @@
+class UnableToGetDocumentsDirectory {}
+
+class DatabaseAlreadyOpenException {}
+
+class DatabaseIsNotOpen {}
+
+class CouldNotDeleteUser {}
+
+class UserAlreadyExits {}
+
+class CouldNotFindUser {}
+
+class CouldNotDeleteNote {}
+
+class CouldNotFindNote {}
+
+class CouldNotUpdateNote {}
